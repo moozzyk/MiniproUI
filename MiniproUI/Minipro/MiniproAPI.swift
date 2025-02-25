43b93a581e80f10b1f3c5e369924fc1c746d9f2b
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2025-02-24 21:31:54 -0800

    Prevent hangs when Programmer not connected
    Some operations (e.g. list devices) ask for the version of the programmer if not connected. As they wait for the user to enter the answer, the process hangs. Trying to get the programmer info throws if the programmer is not connected and can be used to prevent hangs.

--- MiniproUI/Minipro/MiniproAPI.swift
+++ MiniproUI/Minipro/MiniproAPI.swift
@@ -15,17 +15,22 @@ enum APIError: Error, Equatable {
 }
 
 class MiniproAPI {
+    private static func ensureProgrammerConnected() async throws {
+        let _ = try await getProgrammerInfo()
+    }
     static func getProgrammerInfo() async throws -> ProgrammerInfo {
         let result = try await MiniproInvoker.invoke(arguments: ["-t"])
         return try ProgrammerInfoProcessor.run(result)
     }
 
     static func getSupportedDevices() async throws -> [String] {
+        try await ensureProgrammerConnected()
         let result = try await MiniproInvoker.invoke(arguments: ["-l"])
         return try SupportedDevicesProcessor.run(result)
     }
 
     static func getDeviceDetails(device: String) async throws -> DeviceDetails {
+        try await ensureProgrammerConnected()
         let result = try await MiniproInvoker.invoke(arguments: ["-d", device])
         return try DeviceDetailsProcessor.run(result)
     }
