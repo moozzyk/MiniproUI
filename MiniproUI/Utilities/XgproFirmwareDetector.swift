cd9fb5063add8b0048b969b4ee4fb65f5883f7f6
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2026-02-07 14:09:39 -0800

    Add firmware detector

---
+++ MiniproUI/Utilities/XgproFirmwareDetector.swift
@@ -0,0 +1,49 @@
+//
+//  XgproFirmwareDetector.swift
+//  MiniproUI
+//
+//  Created by Pawel Kadluczka on 2/1/26.
+//
+
+import Foundation
+
+enum XgproFirmwareDetectorError: Error {
+    case firmwareNotFound
+}
+
+class XgproFirmwareDetector {
+    private static let t56FileName = "updatet56.dat"
+    private static let t76FileName = "updatet76.dat"
+
+    enum FirmwareTarget {
+        case t56(file: URL)
+        case t76(file: URL)
+    }
+
+    public static func detectFirmwareTarget(in folder: URL) throws -> FirmwareTarget {
+        let entries = try FileManager.default.contentsOfDirectory(
+            at: folder,
+            includingPropertiesForKeys: nil,
+            options: [.skipsHiddenFiles]
+        )
+
+        var t56Match: URL?
+        var t76Match: URL?
+        for entry in entries {
+            let name = entry.lastPathComponent.lowercased()
+            if name == t76FileName {
+                t76Match = entry
+            } else if name == t56FileName {
+                t56Match = entry
+            }
+        }
+
+        if let t76Match {
+            return .t76(file: t76Match)
+        }
+        if let t56Match {
+            return .t56(file: t56Match)
+        }
+        throw XgproFirmwareDetectorError.firmwareNotFound
+    }
+}
