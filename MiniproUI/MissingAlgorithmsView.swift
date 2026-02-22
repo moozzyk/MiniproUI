9ed108f2e898cf5308ad21556641dbbf62dbfb29
Pawel Kadluczka <1438884+moozzyk@users.noreply.github.com>
2026-02-21 22:32:01 -0800

    Add MissingAlgorithms view

---
+++ MiniproUI/MissingAlgorithmsView.swift
@@ -0,0 +1,18 @@
+//
+//  MissingAlgorithmsView.swift
+//  MiniproUI
+//
+
+import SwiftUI
+
+struct MissingAlgorithms: View {
+    var body: some View {
+        ErrorBanner(
+            errorMessage: "Additional setup required. Install missing artifacts in Settings."
+        )
+    }
+}
+
+#Preview {
+    MissingAlgorithms()
+}
